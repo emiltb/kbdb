9cfc3ab7b7f0aa2f93772ba82c8999498619bf0e
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-07-08 20:29:07 +0200

    Initial commit - table working

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProgramTableComponent } from './program-table/program-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProgramTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
